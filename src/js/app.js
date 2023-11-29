a91d2e5b9fcda519d8e507137c9b5fa6a35e4a42
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-11-29 19:38:08 +0100

    custom SPA

---
+++ src/js/app.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  document.querySelectorAll('a').forEach(function(link) {
+    // console.log(link.hash.split("#")[1])
+    if (link.host !== window.location.host || !link.hash.split("#")[1])
+      return;
+
+    link.addEventListener('click', function(event) {
+      event.preventDefault();
+      history.pushState(null, null, this.href);
+      showPage(link.href.split('#')[1]);
+    });
+  });
+};
+
+function showPage(url) {
+  console.log(url);
+  document.querySelectorAll('div').forEach(function(page) {
+    page.style.display = 'none';
+  });
+  document.querySelector('#' + url).style.display = 'block';
+}
