977ac7a38d91c26b20f697f56c19144e71fa8cc4
Aimé <aimelimhountchen@gmail.com>
2023-12-02 22:23:19 +0100

    addTrainingPictures : refractor by using getPictures

--- src/js/pages/training.js
+++ src/js/pages/training.js
@@ -1,11 +1,16 @@
-import {Picture} from "../classes/Picture.js";
+import {getPictures, Picture} from "../classes/Picture.js";
 
 /**
  * Add training pictures to the page
  */
 export function addTrainingPictures() {
-  const picture = new Picture('biche', 'assets/grids/animals-1/biche.jpg');
-  picture.addPicture('.cell:nth-of-type(1)>img');
-  document.querySelector('.cell:nth-of-type(1)')
-    .addEventListener('click', picture.sayPictureName.bind(picture));
+  // const picture = new Picture('biche', 'assets/grids/animals-1/biche.jpg');
+  // picture.addPicture('.cell:nth-of-type(1)>img');
+  // document.querySelector('.cell:nth-of-type(1)')
+  //   .addEventListener('click', picture.sayPictureName.bind(picture));
+  getPictures().forEach((picture, index) => {
+    picture.addPicture(`.cell:nth-of-type(${index + 1})>img`);
+    document.querySelector(`.cell:nth-of-type(${index + 1})`)
+      .addEventListener('click', picture.sayPictureName.bind(picture));
+  });
 }
