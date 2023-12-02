3af53ee5f465a53b3629fdf33a288461bc1fe25f
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-12-02 12:07:43 +0100

    tests for implemented Grid

--- tests/js/classes/Grid.test.js
+++ tests/js/classes/Grid.test.js
@@ -1,25 +1,9 @@
 const { JSDOM } = require('jsdom');
 const { Grid } = require('../../../src/js/elements/gameGrid/Grid');
+const {NotImplementedError} = require("../../../src/js/application/errors/NotImplementedError");
 
 describe('Grid', () => {
   test('constructor', () => {
-    const dom = new JSDOM(`<!DOCTYPE html><div id="parent"></div>`);
-    global.document = dom.window.document;
-
-    const grid = new Grid('testGrid', '#parent');
-    expect(grid.gridName).toBe('testGrid');
-    expect(grid.parentSelector).toBe(document.querySelector('#parent'));
-    expect(grid.grid).toBe(document.querySelector('#testGrid'));
-  });
-
-  test('setCells', () => {
-    const dom = new JSDOM(`<!DOCTYPE html><div id="parent"></div>`);
-    global.document = dom.window.document;
-
-    const grid = new Grid('testGrid', '#parent');
-    grid.initCells(3);
-
-    const cells = document.querySelectorAll('.cell');
-    expect(cells.length).toBe(9);
+    expect(() => {new Grid('testGrid', '#parent')}).toThrow(NotImplementedError);
   });
 });
