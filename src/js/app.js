75e096a6a89734c692d3b2555c7ec4eb0c03c3ba
Aimé <aimelimhountchen@gmail.com>
2023-12-02 23:03:06 +0100

    app.js : call addEvaluationPictures

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 import {initRouter} from "./application/router.js";
 import {addTrainingPictures} from "./pages/training.js";
+import {addEvaluationPictures} from "./pages/gridGame.js";
+
 
 initRouter();
 addTrainingPictures();
+addEvaluationPictures();
