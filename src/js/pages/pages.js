d3b56d0ae47a246fad44c0955c509eec195b8ada
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-12-02 03:28:57 +0100

    cleaning code

--- src/js/pages/pages.js
+++ src/js/pages/pages.js
@@ -6,5 +6,4 @@ export function initPagesContent() {
 
 function initTrainingPage() {
   appendGrid();
-  // addTrainingPictures();
 }
--- src/js/pages/training.js
+++ src/js/pages/training.js
@@ -1,19 +1,9 @@
-import {Picture} from "../elements/Picture.js";
 import {Grid} from "../elements/Grid.js";
 
-/**
- * Add training pictures to the page
- */
-export function addTrainingPictures() {
-  const picture = new Picture('biche', 'assets/grids/animals-1/biche.jpg');
-  picture.addPicture('.cell:nth-of-type(1)>img');
-  document.querySelector('.cell:nth-of-type(1)')
-    .addEventListener('click', picture.sayPictureName.bind(picture));
-}
-
-export function appendGrid() {
+export function appendGrid() { //appendgrid(Serie)
   const grid = new Grid('grid', '#learn > .grid-container');
   grid.initCells(3);
+  // TODO: remplacer imageList par un objet Serie
   grid.setCellsImages('assets/grids/animals-1/', [
     {name: 'biche.jpg', alt: 'biche'},
     {name: 'chat.jpg', alt: 'chat'},
