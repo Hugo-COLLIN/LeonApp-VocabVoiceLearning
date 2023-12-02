5b1ada98665fe9a255eec92e5f4a7d72cf83953c
Aimé <aimelimhountchen@gmail.com>
2023-12-02 22:59:21 +0100

    addTrainingPictures : only for learn page

--- src/js/pages/training.js
+++ src/js/pages/training.js
@@ -1,16 +1,12 @@
-import {getPictures, Picture} from "../classes/Picture.js";
+import {getPictures} from "../classes/Picture.js";
 
 /**
  * Add training pictures to the page
  */
 export function addTrainingPictures() {
-  // const picture = new Picture('biche', 'assets/grids/animals-1/biche.jpg');
-  // picture.addPicture('.cell:nth-of-type(1)>img');
-  // document.querySelector('.cell:nth-of-type(1)')
-  //   .addEventListener('click', picture.sayPictureName.bind(picture));
   getPictures().forEach((picture, index) => {
-    picture.addPicture(`.cell:nth-of-type(${index + 1})>img`);
-    document.querySelector(`.cell:nth-of-type(${index + 1})`)
+    picture.addPicture(`#learn .grid .cell:nth-of-type(${index + 1})>img`);
+    document.querySelector(`#learn .grid .cell:nth-of-type(${index + 1})`)
       .addEventListener('click', picture.sayPictureName.bind(picture));
   });
 }
