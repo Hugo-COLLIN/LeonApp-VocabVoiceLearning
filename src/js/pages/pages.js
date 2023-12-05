7dddd6967e9d11de17f56d39d1c2534744de2fa9
Aimé <aimelimhountchen@gmail.com>
2023-12-05 21:06:36 +0100

    update cells when <a> is clicked

--- src/js/pages/pages.js
+++ src/js/pages/pages.js
@@ -1,28 +1,49 @@
-import {appendGrid} from "./training.js";
-import {initGameGrid} from "./game.js";
+// import {appendGrid} from "./training.js";
+
 import {getAnimalGameSet, getFruitGameSet} from "../elements/GameSet.js";
+import {Game} from "./game.js";
 
 let gameSet = getAnimalGameSet();
-if (window.location.hash === '#fruits') {
-  gameSet = getFruitGameSet();
-}
+const game = new Game(gameSet);
 
 export function initPagesContent() {
-  initTrainingPage();
+  // initTrainingPage();
   initGamePage();
+
+  // Listen for hash changes
+  document.querySelectorAll('a').forEach(function (link) {
+    // Only listen for internal links
+    if (link.host !== window.location.host || !link.hash.split("#")[1])
+      return;
+
+    // Listen for clicks on the links
+    link.addEventListener('click', function (event) {
+      game.updateGameSet(gameSet);
+      switch (event.target.hash) {
+        case '#animals':
+          gameSet = getAnimalGameSet();
+          break;
+        case '#fruits':
+          gameSet = getFruitGameSet();
+          break;
+      }
+    });
+  });
 }
 
-function initTrainingPage() {
-  appendGrid(gameSet);
-}
+// function initTrainingPage() {
+//   appendGrid(gameSet);
+// }
 
 function initGamePage() {
-  const gridGame = initGameGrid(gameSet);
   document.querySelector('#game').addEventListener('show', () => {
-    gridGame.startGame();
+    game.gridGame.startGame();
   });
-  document.querySelector('#game .play-button').addEventListener('click', () => {
-    gridGame.saySelectedPictureName();
+  // document.querySelector('#game .play-button').addEventListener('click', () => {
+  //   game.gridGame.saySelectedPictureName();
+  // });
+  game.gridGame.cells.forEach(cell => {
+    cell.setCellOralMessage();
   });
 }
 
