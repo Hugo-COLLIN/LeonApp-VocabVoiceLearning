a00e2b38c258d383e481ccf25de8fea15597cfb9
Aimé <aimelimhountchen@gmail.com>
2023-12-05 21:04:37 +0100

    added class Game updateGameSet : sets game and training grid

--- src/js/pages/game.js
+++ src/js/pages/game.js
@@ -1,10 +1,20 @@
 import {GridGame} from "../elements/gameGrid/GridGame.js";
+import {GridLearning} from "../elements/gameGrid/GridLearning.js";
 
-export function initGameGrid(gameSet) {
-  const gridGame = new GridGame('gridGame', '#game > .grid-container', gameSet);
-  gridGame.initCells(3);
-  gridGame.fillCells(gameSet);
+export class Game {
+    constructor(gameSet) {
+        this.gameSet = gameSet;
+        this.gridGame = new GridGame('gridGame', '#game > .grid-container', gameSet);
+        this.gridGame.initCells(3);
+        this.gridGame.fillCells(this.gameSet);
+        this.grid = new GridLearning('grid', '#learn > .grid-container');
+        this.grid.initCells(3);
+        this.grid.fillCells(gameSet);
+    }
 
-  return gridGame;
+    updateGameSet(gameSet) {
+        this.gameSet = gameSet;
+        this.gridGame.updateCells(this.gameSet);
+        this.grid.updateCells(this.gameSet)
+    }
 }
-
