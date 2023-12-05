bdd998ec2cc8761385d17db2626f24c611692e2a
Aimé <aimelimhountchen@gmail.com>
2023-12-05 21:07:02 +0100

    removed appendGrid => now in class Game

--- src/js/pages/training.js
+++ src/js/pages/training.js
@@ -1,12 +1,11 @@
 import {GridLearning} from "../elements/gameGrid/GridLearning.js";
 
-export function appendGrid(gameSet) {
-  const grid = new GridLearning('grid', '#learn > .grid-container');
-  grid.initCells(3);
-
-  grid.fillCells(gameSet);
-}
-
+// const grid = new GridLearning('grid', '#learn > .grid-container');
+//
+// export function appendGrid(gameSet) {
+//   grid.initCells(3);
+//   grid.fillCells(gameSet);
+// }
 
 //[
 //     {name: 'biche.jpg'},
