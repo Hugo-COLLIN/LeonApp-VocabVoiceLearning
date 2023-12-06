902e1a0beb04919698551075efc9ff822c9aee64
Aimé <aimelimhountchen@gmail.com>
2023-12-06 21:25:40 +0100

    jsonToGameSet : return GameSet object from jsonpath

--- src/js/elements/GameSet.js
+++ src/js/elements/GameSet.js
@@ -5,3 +5,17 @@ export class GameSet {
     }
 }
 
+/**
+ * return a GameSet object from a json file
+ * @param jsonpath
+ */
+export function jsonToGameSet(jsonpath) {
+    const gameSet = new GameSet();
+    fetch(jsonpath)
+        .then((response) => response.json())
+        .then((data) => {
+            gameSet.path = data.path;
+            gameSet.imageList = data.imageList;
+        });
+    return gameSet;
+}
