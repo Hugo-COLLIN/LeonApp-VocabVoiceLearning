fb093f0a817152320e0792f48ce4845e31f38ad0
Aimé <aimelimhountchen@gmail.com>
2023-12-07 09:49:54 +0100

    modified constructor

--- src/js/elements/GameSet.js
+++ src/js/elements/GameSet.js
@@ -1,21 +1,10 @@
 export class GameSet {
-    constructor(path, imageList) {
-        this.path = path;
-        this.imageList = imageList;
+    constructor(jsonpath) {
+      fetch(jsonpath)
+          .then((response) => response.json())
+          .then((data) => {
+            this.path = data.path;
+            this.imageList = data.imageList;
+          });
     }
-}
-
-/**
- * return a GameSet object from a json file
- * @param jsonpath
- */
-export function jsonToGameSet(jsonpath) {
-    const gameSet = new GameSet();
-    fetch(jsonpath)
-        .then((response) => response.json())
-        .then((data) => {
-            gameSet.path = data.path;
-            gameSet.imageList = data.imageList;
-        });
-    return gameSet;
 }
